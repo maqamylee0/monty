e9df291375399ed17ced78789580cc53f6bf3650
maqamylee0 <maqamylee0@gmail.com>
2023-05-25 20:05:18 -0700

    add

---
+++ nop.c
@@ -0,0 +1,28 @@
+/**
+ * nop- does nothing
+ * @stack: stack
+ * @line_number: line number
+ * Return: tothing
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+	(void *)stack;
+}
+
+/**
+ * add- adds top elements of stack
+ * @stack: stack
+ * @line_number
+ * Return: nothing
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL  || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n += (*stack)->n;
+	pop(&stack, line_number);
+}
